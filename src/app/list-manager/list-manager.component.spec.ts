fd0f5027e7a731397a2c8341bec78c8ed50899fa
hkst17 <slatt@ucsc.edu>
2020-10-13 23:44:05 -0700

    added some css and list saves even refreshed

--- src/app/list-manager/list-manager.component.spec.ts
+++ src/app/list-manager/list-manager.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ListManagerComponent } from './list-manager.component';
 
@@ -6,12 +6,12 @@ describe('ListManagerComponent', () => {
   let component: ListManagerComponent;
   let fixture: ComponentFixture<ListManagerComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
       declarations: [ ListManagerComponent ]
     })
     .compileComponents();
-  });
+  }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ListManagerComponent);
